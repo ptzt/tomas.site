2afb4b1527ab73d627eaa5f963c16dbf86ecb33a
ptzt <ignaciotomasrodriguezmillan@gmail.com>
2025-02-17 18:52:55 -0300

    working map in mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,35 @@ import dynamic from "next/dynamic";
 const MapCard = dynamic(() => import("@/components/MapCard"), { ssr: false });
 
 export default function Home() {
-
   return (
-    <div className="min-h-screen flex items-center justify-center px-4 py-4 ">
-      <div className="grid grid-cols-4 grid-rows-3 gap-4">
-        <div className="col-span-2 row-span-3 col-start-1 bg-slate-100 rounded-xl w-full p-4">
+    <div className="min-h-screen flex items-center justify-center px-4 py-4">
+      {/* Móvil: Stack vertical | Desktop: Grid */}
+      <div className="flex flex-col gap-4 w-full  lg:grid lg:grid-cols-4 lg:grid-rows-3">
+
+        {/* Profile Card - Ocupa toda la izquierda en Desktop, Full en Mobile */}
+        <div className="bg-slate-100 rounded-xl w-full p-4 col-span-2 row-span-3">
           <ProfileCard />
         </div>
-        <div className="col-span-2 col-start-3 bg-slate-100 rounded-xl w-full p-4"><GithubCard /></div>
-        <div className="row-span-2 col-start-3 row-start-2 bg-slate-100 rounded-xl w-full p-4"><MapCard /></div>
-        <div className="col-start-4 row-start-2 bg-slate-100 rounded-xl w-full p-4"><LeetCodeCard /></div>
-        <div className="col-start-4 row-start-3">Spofify card?</div>
+
+        {/* GitHub Card */}
+        <div className="bg-slate-100 rounded-xl w-full p-4 lg:col-span-2 lg:col-start-3">
+          <GithubCard />
+        </div>
+
+        {/* Map Card */}
+        <div className="bg-slate-100 rounded-xl w-full p-4 lg:row-span-2 lg:col-start-3 lg:row-start-2">
+          <MapCard />
+        </div>
+
+        {/* LeetCode Card */}
+        <div className="bg-slate-100 rounded-xl w-full p-4 lg:col-start-4 lg:row-start-2">
+          <LeetCodeCard />
+        </div>
+
+        {/* Spotify Card */}
+        <div className="bg-slate-100 rounded-xl w-full p-4 lg:col-start-4 lg:row-start-3">
+          <p>Spotify Card?</p>
+        </div>
       </div>
     </div>
   );
--- src/components/MapCard.tsx
+++ src/components/MapCard.tsx
@@ -3,18 +3,24 @@ import { Map, Marker, Annotation } from "mapkit-react";
 
 export default function MapCard() {
   const token: string = process.env.NEXT_PUBLIC_MAP_TOKEN || "";
-  const initialPosition = {
+  const initialRegion = {
     centerLatitude: -34.60,
     centerLongitude: -58.39,
-    latitudeDelta: 0.15,
-    longitudeDelta: 0.15
+    latitudeDelta: 0.25,
+    longitudeDelta: 0.25
   }
-
   return (
-    <Map token={token} initialRegion={initialPosition} showsZoomControl={false} showsCompass={0} showsMapTypeControl={false}>
-      <Annotation latitude={-34.60} longitude={-58.39} animates appearanceAnimation="gelatine 0.5s infinite" >
-        <Marker latitude={-34.60} longitude={-58.39} />
-      </Annotation>
-    </Map>
+    <div style={{ width: '100%', height: '536px' }}>
+      <Map
+        token={token}
+        initialRegion={initialRegion}
+        minCameraDistance={100}
+        maxCameraDistance={5000}
+      >
+        <Annotation latitude={-34.60} longitude={-58.39} animates appearanceAnimation="gelatine 0.5s infinite">
+          <Marker latitude={-34.60} longitude={-58.39} />
+        </Annotation>
+      </Map>
+    </div>
   );
 }
